abbf984ea8d046aa61a4009d35b7ebc5fd759df1
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-10-10 07:53:10 +0100

    solve stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,14 +4,25 @@ import argparse
 
 
 def find_max_profit(prices):
-  for price in prices:
-    print(price)
+    maxProfit = 0
+
+    for i in range(0, len(prices)):
+        for j in range(i+1, len(prices)):
+            if (i+1 < len(prices)):
+                if (maxProfit == 0):
+                    maxProfit = prices[j] - prices[i]
+                if (maxProfit < prices[j] - prices[i]):
+                    maxProfit = prices[j] - prices[i]
+
+    return maxProfit
 
 
 print(find_max_profit([1050, 270, 1540, 3800, 2]))
+# print(find_max_profit([100, 90, 80, 50, 20, 10]))
+
 
 if __name__ == '__main__':
-    # This is just some code to accept inputs from the command line
+        # This is just some code to accept inputs from the command line
     parser = argparse.ArgumentParser(
         description='Find max profit from prices.')
     parser.add_argument('integers', metavar='N', type=int,
