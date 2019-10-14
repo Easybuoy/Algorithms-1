04cc1283d4f6d9a92418e7f11400f7dde8b254d1
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2019-10-14 09:18:22 +0100

    Revert "Algorithms"

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,17 +2,13 @@
 
 import sys
 
-
 def rock_paper_scissors(n):
-  print(n)
-  pass
+  pass 
 
-# [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
-print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
-    num_plays=int(sys.argv[1])
+    num_plays = int(sys.argv[1])
     print(rock_paper_scissors(num_plays))
   else:
     print('Usage: rps.py [num_plays]')
